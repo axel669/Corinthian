ca5152ea1b3912dc82981c152d55041674bfb74a
axel669 <haryuu.hanekata@gmail.com>
2016-05-13 19:40:37 -0700

    default style more like material, more options

--- lib-source/ui/progressbar.js
+++ lib-source/ui/progressbar.js
@@ -5,24 +5,26 @@ Style.create(
     {
         ".wrapper": {
             position: 'relative',
-            height: 30,
+            height: 8,
             overflow: 'hidden',
-            borderRadius: 15,
-            border: '4px solid #e4e4e4'
+            backgroundColor: Theme.get("progressbar/backgroundColor"),
+            // borderRadius: 15,
+            // border: '4px solid #e4e4e4'
+            border: '1px solid rgba(200, 200, 200, 0.5)'
         },
         ".bar": {
             height: '100%',
             position: 'absolute',
             left: 0,
             top: 0,
-            backgroundColor: '#87c3e2'
+            backgroundColor: Theme.get("progressbar/color")
         }
     }
 );
 
-const ProgressBar = ({progress, cornerRadius}) => (
-    <div className={Style.getClassName("core/progressbar:wrapper")}>
-        <div className={Style.getClassName("core/progressbar:bar")} style={{width: `${progress * 100}%`}} />
+const ProgressBar = ({progress, color, backgroundColor, height, cornerRadius}) => (
+    <div className={Style.getClassName("core/progressbar:wrapper")} style={{backgroundColor, height, borderRadius: cornerRadius}}>
+        <div className={Style.getClassName("core/progressbar:bar")} style={{width: `${progress * 100}%`, backgroundColor: color}} />
     </div>
 );
 
